259591b2e7d72b68a8caaf5dbbc7f9d96191e31e
Moyosore <msontop.me@gmail.com>
2023-03-18 19:32:55 +0100

    Hashing Password

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_migrate import Migrate
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
@@ -29,6 +29,18 @@ class Users(db.Model):
     email = db.Column(db.String(200), nullable=False, unique=True)
     favorite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    #Password workings
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attr!')
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash,password)
 
 #Create A String
 def __repr__(self):
