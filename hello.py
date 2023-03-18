92f9d18e5198f7c66bb83080627603e9adb089a9
Moyosore <msontop.me@gmail.com>
2023-03-18 13:14:46 +0100

    webforms and messages

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
 
 #Create a flask instance 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my supereei10'
+
+#Create a form class
+class NamerForm(FlaskForm):
+    name = StringField('What\'s your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 #Create a route decorator
 @app.route('/')
@@ -17,6 +28,20 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', user_name=name)
+
+#Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #Validate Form 
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash('Form submitted successfully!')
+    return render_template('name.html',
+                           name = name,
+                           form = form)
 
 #Create custom error pages
 
